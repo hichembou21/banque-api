e7863fac56187f311d478a9532addd2adbf6ae13
bouaffarh <hichem.bouaffar@gmail.com>
2018-11-10 22:17:15 +0100

    ajout employeForm

--- src/main/java/org/hich/services/EmployeRestService.java
+++ src/main/java/org/hich/services/EmployeRestService.java
@@ -2,8 +2,12 @@ package org.hich.services;
 
 import java.util.List;
 
+import org.hich.entities.AppUser;
+import org.hich.entities.Client;
 import org.hich.entities.Employe;
 import org.hich.metier.EmployeMetier;
+import org.hich.services.util.EmployeForm;
+import org.hich.services.util.RegisterForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +28,22 @@ public class EmployeRestService {
 	private EmployeMetier employeMetier;
 
 	@RequestMapping(method=RequestMethod.POST)
-	public Employe addEmploye(@RequestBody Employe employe) {
-		return accountService.saveEmploye(employe);
+	public Employe addEmploye(@RequestBody EmployeForm employeForm) {
+		if (!employeForm.getPassword().equals(employeForm.getConfirmPassword()))
+			throw new RuntimeException("You Must Comfirm the password");
+
+		AppUser user = accountService.findUserByUsername(employeForm.getUsername());
+		if (user != null) throw new RuntimeException("User already exist");
+
+		Employe employe = new Employe();
+		employe.setName(employeForm.getName());
+		employe.setUsername(employeForm.getUsername());
+		employe.setPassword(employeForm.getPassword());
+		employe.setEmployeSup(employeForm.getEmployeSup());
+		accountService.saveEmploye(employe);
+		accountService.addRoleToUser(employeForm.getUsername(), "EMPLOYE");
+		accountService.addRoleToUser(employeForm.getUsername(), "USER");
+		return employe;
 	}
 
 	@RequestMapping(method=RequestMethod.GET)
---
+++ src/main/java/org/hich/services/util/EmployeForm.java
@@ -0,0 +1,46 @@
+package org.hich.services.util;
+
+
+import org.hich.entities.Employe;
+
+public class EmployeForm {
+
+    private String name;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private Employe employeSup;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+    public Employe getEmployeSup() {
+        return employeSup;
+    }
+
+    public void setEmployeSup(Employe employeSup) {
+        this.employeSup = employeSup;
+    }
+}
